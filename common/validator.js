94dbfa924dced172c5deae37ff7c31a4eda0c899
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-07-16 09:24:35 +0100

    trying with 403

--- common/validator.js
+++ common/validator.js
@@ -8,7 +8,7 @@ module.exports = {
         return (req, res, next) => {
             const result = Joi.validate(req.body, schema);
             if (result.error) {
-                return res.status(400).json(result.error);
+                return res.status(403).json(result.error);
             }
             if (!req.value) {
                 req.value = {};
