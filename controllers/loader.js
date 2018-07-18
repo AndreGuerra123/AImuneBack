ed026733987bc32a181e9490aa557a2219a290ac
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-07-18 18:19:07 +0100

    formidable

--- controllers/loader.js
+++ controllers/loader.js
@@ -8,6 +8,8 @@ module.exports = {
         var form = new formi.IncomingForm();
 
         form.parse(req, function(err,fields,files){
+            console.log(fields);
+            console.log(files);
             res.writeHead(200, {'content-type': 'text/plain'});
             res.write('received upload:\n\n');
             res.end(util.inspect({fields: fields, files: files}));
