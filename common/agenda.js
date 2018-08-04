0415f3bd2e8a0ae741a31b563f6ba128619ade8a
admin <admin@ceamphd82.campus.ncl.ac.uk>
2018-08-04 20:57:20 +0100

    dada

--- common/agenda.js
+++ common/agenda.js
@@ -1,35 +1,48 @@
-const {MONGO} = require('../config/index.js');
+const {
+    MONGO
+} = require('../config/index.js');
 const Agenda = require('agenda');
 const Modeler = require('../models/models.js');
 
 //Configuring agenda
-let agenda = new Agenda({db: {address: MONGO, collection: 'jobs'}});
+let agenda = new Agenda({
+    db: {
+        address: MONGO,
+        collection: 'jobs'
+    }
+});
 
 
-const updateProgress = async function(job, value, description){
-    await agenda.update({"_id":job._id},
-        {
-            $set: {
-                progress:{
-                    value,
-                    description
-                }
+const updateProgress = async function (jobi, value, description) {
+
+    await agenda.jobs({
+        "_id": jobi._id
+    }, function (err, job) {
+        if(!err){
+            job.progress = {
+                value,
+                description
             }
+            job.save();    
         }
-    )
+    });
 }
 
 //train the model
-agenda.define('train',{priority:'high'},(job,done)=>{
-   let i = 0;
-   while(i<=100){
-    setTimeout(updateProgress(job,i,i),1000);
-   }
-   done();
+agenda.define('train', {
+    priority: 'high'
+}, (job, done) => {
+    let i = 0;
+    while (i <= 100) {
+        setTimeout(updateProgress(job, i, i), 1000);
+    }
+    done();
 });
 
 //Starting agenda
-agenda.on('ready', function() {agenda.start();});
+agenda.on('ready', function () {
+    agenda.start();
+});
 
 //Exporting agenda
 module.exports = agenda;
